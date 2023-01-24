db5fe897ea01687d485716456d68db5c40e41f0e
Jason Taylor <jasongt@outlook.com>
2023-01-24 12:15:59 +0000

    BeyondBasics-06

--- CaWorkshop.WebUI/Controllers/ApiControllerBase.cs
+++ CaWorkshop.WebUI/Controllers/ApiControllerBase.cs
@@ -1,10 +1,13 @@
-﻿using MediatR;
+﻿using CaWorkshop.WebUI.Filters;
+
+using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 
 namespace CaWorkshop.WebUI.Controllers;
 
 [ApiController]
+[ApiExceptionFilter]
 [Route("api/[controller]")]
 public abstract class ApiControllerBase : ControllerBase
 {
--- CaWorkshop.WebUI/Controllers/TodoItemsController.cs
+++ CaWorkshop.WebUI/Controllers/TodoItemsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CaWorkshop.Domain.Entities;
 using CaWorkshop.Infrastructure.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace CaWorkshop.WebUI.Controllers
 {
